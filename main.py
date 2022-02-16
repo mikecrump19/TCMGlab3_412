c2089fdb2066eb257ee3136cf5fda17d52ece245
Michael Crump <98478689+mikecrump19@users.noreply.github.com>
2022-02-16 14:26:59 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import urllib.request
 import pandas as pd
-import csv
 
 
 url = "https://s3.amazonaws.com/tcmg476/http_access_log"
 file = urllib.request.urlopen(url)
 for line in file:
     decoded_line = line.decode("utf-8")
-print(decoded_line)
+#print(decoded_line)
+count = 0
+for i in decoded_line:
+    count += count
+
+print("Total number of logs: ", count)
 
 
